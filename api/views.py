df99b388a74628854f0229e74124e6036694d7ca
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-06 11:34:01 +0900

    Ready to use API communication for getting post detail information in Django

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
 from api.utils import obj_to_post
 
@@ -15,3 +16,13 @@ class ApiPostLV(BaseListView):
         postList = [obj_to_post(obj) for obj in qs]
 
         return JsonResponse(data=postList, safe=False, status=200)
+
+
+class ApiPostDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context["object"]
+        post = obj_to_post(obj)
+
+        return JsonResponse(data=post, safe=True, status=200)

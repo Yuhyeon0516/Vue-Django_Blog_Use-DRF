a31b874b381195ec4edb17c782642a13e757bd3a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-07 23:37:01 +0900

    Changed viewset to genericview

--- api2/views.py
+++ api2/views.py
@@ -1,20 +1,41 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
+# from django.contrib.auth.models import User
+# from rest_framework import viewsets
 
-from api2.serializer import CommentSerializer, PostSerializer, UserSerializer
+# from api2.serializer import CommentSerializer, PostSerializer, UserSerializer
+# from blog.models import Comment, Post
+
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class CommentViewSet(viewsets.ModelViewSet):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from api2.serializer import CommentSerializer, PostSerializer
+
 from blog.models import Comment, Post
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class PostViewSet(viewsets.ModelViewSet):
+class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class CommentViewSet(viewsets.ModelViewSet):
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer

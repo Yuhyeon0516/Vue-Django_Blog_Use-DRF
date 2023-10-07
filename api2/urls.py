c63446c0039d969cd82906c0754aa0348df8e0dd
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-07 23:25:22 +0900

    Created viewset to Comment model.

--- api2/urls.py
+++ api2/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api2.views import PostViewSet, UserViewSet
+from api2.views import CommentViewSet, PostViewSet, UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r"user", UserViewSet)
 router.register(r"post", PostViewSet)
+router.register(r"comment", CommentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
